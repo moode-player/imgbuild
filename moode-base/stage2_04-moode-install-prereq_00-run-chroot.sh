ab8a1949de87fdebfe5beab602f9456185c4f0d6
bitkeeper <bitkeeper@users.noreply.github.com>
2022-02-13 12:23:18 +0100

    Added config for moode-base img
    This is a image which include all deps of moode-player, but not moode-player it self. Makes it easier to test clean installs.

---
+++ moode-base/stage2_04-moode-install-prereq_00-run-chroot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Installing cloudsmith repository"
+
+# Normal install will not work due Warning: apt-key is deprecated:
+# curl -1sLf \
+# 'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' \
+# | sudo -E bash
+
+# Using gpg --dearmor instead
+KEYRING=/usr/share/keyrings/moodeaudio_m8y.gpg
+curl -fsSL 'https://dl.cloudsmith.io/public/moodeaudio/m8y/gpg.E5A251A46C58117E.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/E5A251A46C58117E.gpg > /dev/null
+curl -fsSL 'https://dl.cloudsmith.io/public/moodeaudio/m8y/config.deb.txt?distro=raspbian&codename=bullseye' > /etc/apt/sources.list.d/moodeaudio-m8y.list
+
+echo "Update package lists"
+apt-get update
+
+KERNEL_VER=$(basename `ls -d /lib/modules/*-v7l+` | sed -r "s/([0-9.]*)[-].*/\1/")
+echo "Kernel $KERNEL_VER detected, try to install drivers"
+apt-get install -y "aloop-$KERNEL_VER" "pcm1794a-$KERNEL_VER" "ax88179-$KERNEL_VER" "rtl88xxau-$KERNEL_VER"
---
+++ moode-base/stage2_06-moode-install-post_00-run-chroot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Post moode-player install"
+# sudo moode-apt-mark hold
