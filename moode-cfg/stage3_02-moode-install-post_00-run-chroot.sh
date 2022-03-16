6de3b89d0bfbd990c49feed0dea70366c798bcd3
Tim Curtis <tim@moodeaudio.org>
2022-03-16 15:37:54 -0400

    Bump to kernel 5.15.28

--- moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
+++ moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
@@ -8,13 +8,12 @@
 #
 #########################################################################
 
-# kernel hashes for rpi-update
-# 5.15.24 77451e601cbee4835b04ccf2d583c53be6e21454
-# 5.15.23 9e99d81910722b6f2e086f6d14e592f292bcbb8b
-# 5.15.21 fbcab73d5a20f592aa9f6b0e364757ef131dae27
+# Kernel hashes for rpi-update
+# r801: 5.15.28 87c6654a59e0ae6d09869fffceb44c5c698a7d83
+# r800: 5.15.23 9e99d81910722b6f2e086f6d14e592f292bcbb8b
 
 # Install kernel from rpi-update
-echo "y" | sudo PRUNE_MODULES=1 rpi-update 9e99d81910722b6f2e086f6d14e592f292bcbb8b
+echo "y" | sudo PRUNE_MODULES=1 rpi-update 87c6654a59e0ae6d09869fffceb44c5c698a7d83
 
 # Install matching kernel drivers
 KERNEL_VER=$(basename `ls -d /lib/modules/*-v7l+` | sed -r "s/([0-9.]*)[-].*/\1/")
