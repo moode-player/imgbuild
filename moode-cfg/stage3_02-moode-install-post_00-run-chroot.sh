7c972a854e5b7be4405f2249f6b23e2c74da5fdb
bitkeeper <bitkeeper@users.noreply.github.com>
2022-04-17 09:56:34 +0100

    Now 5.15.x is a available in the repos, stop using rpi-update for kernels.

--- moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
+++ moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
@@ -9,14 +9,15 @@
 #########################################################################
 
 # Kernel hashes for rpi-update
+# r810pre1: 5.15.32 stock
 # r801: 5.15.28 87c6654a59e0ae6d09869fffceb44c5c698a7d83
 # r800: 5.15.23 9e99d81910722b6f2e086f6d14e592f292bcbb8b
 
 # Install kernel from rpi-update
-echo "y" | sudo PRUNE_MODULES=1 rpi-update 87c6654a59e0ae6d09869fffceb44c5c698a7d83
+#echo "y" | sudo PRUNE_MODULES=1 rpi-update 87c6654a59e0ae6d09869fffceb44c5c698a7d83
 
 # Install matching kernel drivers
-KERNEL_VER=$(basename `ls -d /lib/modules/*-v7l+` | sed -r "s/([0-9.]*)[-].*/\1/")
+KERNEL_VER=$(basename `ls -d /lib/modules/*-v8+` | sed -r "s/([0-9.]*)[-].*/\1/")
 echo "Kernel $KERNEL_VER detected, try to install drivers"
 apt-get install -y "aloop-$KERNEL_VER" "pcm1794a-$KERNEL_VER" "ax88179-$KERNEL_VER" "rtl88xxau-$KERNEL_VER"
 
