ac0315ba4f3490bc12f2c011cdd506662e239b66
bitkeeper <bitkeeper@users.noreply.github.com>
2022-06-07 20:17:01 +0100

    Revert to fixed install scripted  of cloudsmith repo

--- moode-cfg/stage3_00-moode-install-prereq_00-run-chroot.sh
+++ moode-cfg/stage3_00-moode-install-prereq_00-run-chroot.sh
@@ -8,17 +8,11 @@
 #
 #########################################################################
 
-# echo "Installing cloudsmith repository"
+echo "Installing cloudsmith repository"
 
-# Normal install will not work due Warning: apt-key is deprecated:
-# curl -1sLf \
-# 'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' \
-# | sudo -E bash
-
-# Using gpg --dearmor instead
-# KEYRING=/usr/share/keyrings/moodeaudio_m8y.gpg
-curl -fsSL 'https://dl.cloudsmith.io/public/moodeaudio/m8y/gpg.E5A251A46C58117E.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/E5A251A46C58117E.gpg > /dev/null
-curl -fsSL 'https://dl.cloudsmith.io/public/moodeaudio/m8y/config.deb.txt?distro=raspbian&codename=bullseye' > /etc/apt/sources.list.d/moodeaudio-m8y.list
+curl -1sLf \
+'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' \
+| sudo -E bash
 
 echo "Update package lists"
 apt-get update
