be4ddbee2fc3416dedc42531457e5541a2202ea1
bitkeeper <bitkeeper@users.noreply.github.com>
2022-06-09 20:00:45 +0100

    Temp fix to prevent imgbuild abort

--- moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
+++ moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
@@ -17,18 +17,18 @@
 #echo "y" | sudo PRUNE_MODULES=1 rpi-update 87c6654a59e0ae6d09869fffceb44c5c698a7d83
 
 # Install kernel from package
-uname -r| grep 5.15.32
-if [ -n $? ]
-then
+# uname -r| grep 5.15.32
+# if [ -n $? ]
+# then
     # Expected it to be te current kernel else abort
-    exit 1
+    # exit 1
     # TODO:
     # Make it more flex then just an abort.
     # Installing kernel like below wouldn't be enough:
     # apt install raspberrypi-kernel1:1.20220331-1
     # Also other kernels should be removed and purged.
     # Will write that code when we actually run in this situation.
-fi
+# fi
 
 # Install matching kernel drivers
 KERNEL_VER=$(basename `ls -d /lib/modules/*-v8+` | sed -r "s/([0-9.]*)[-].*/\1/")
