482016c9160595cd7ee73928898d61267a8f192c
Tim Curtis <tim@moodeaudio.org>
2023-04-05 07:27:40 -0400

    [ax88179] Remove from build
    NOTE: Starting with kernel 6.1.y the custom Allo ASIX ax88179 driver is no longer installed due to build breakage in the driver which has been unmaintained since mid-2022. Instead the stock ASIX ax88179 driver is used.

--- moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
+++ moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
@@ -31,9 +31,12 @@
 # fi
 
 # Install matching kernel drivers
+# NOTE: Starting with kernel 6.1.y the custom Allo ASIX ax88179 driver is no longer installed
+# due to build breakage in the driver which has been unmaintained since mid-2022. Instead the
+# stock ASIX ax88179 driver is used.
 KERNEL_VER=$(basename `ls -d /lib/modules/*-v8+` | sed -r "s/([0-9.]*)[-].*/\1/")
 echo "Kernel $KERNEL_VER detected, try to install drivers"
-apt-get install -y "aloop-$KERNEL_VER" "pcm1794a-$KERNEL_VER" "ax88179-$KERNEL_VER" "rtl88xxau-$KERNEL_VER"
+apt-get install -y "aloop-$KERNEL_VER" "pcm1794a-$KERNEL_VER" "rtl88xxau-$KERNEL_VER"
 
 # Lock important package from upgrades etc
 echo "Post moode-player install"
