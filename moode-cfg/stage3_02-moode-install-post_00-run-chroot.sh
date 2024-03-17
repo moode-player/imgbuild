871fbdef1bb41a0de3877c5148834e52bba1e9d4
Tim Curtis <tim@moodeaudio.org>
2024-03-17 13:13:30 -0400

    Get kernel driver install working

--- moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
+++ moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
@@ -9,16 +9,16 @@
 #########################################################################
 
 # Install matching kernel drivers
+
 # NOTE: Starting with kernel 6.1.y the custom Allo ASIX ax88179 driver is no longer installed
 # due to build breakage in the driver which has been unmaintained since mid-2022. Instead the
 # stock ASIX ax88179 driver is used.
 
-#TODO: fix driver install
-# KERNEL_VER=$(basename `ls -d /lib/modules/*-v8+` | sed -r "s/([0-9.]*)[-].*/\1/")
-# echo "Kernel $KERNEL_VER detected, try to install drivers"
-# apt-get install -y "aloop-$KERNEL_VER" "pcm1794a-$KERNEL_VER" "rtl88xxau-$KERNEL_VER"
-
-# Lock important package from upgrades etc
-echo "Post moode-player install"
-#TODO: uncomment moode-apt-mark when moode-player package is also installed
-# sudo moode-apt-mark hold
+# Driver install
+# Use code from rbl_get_kernel_source() for getting kernel version
+_KERNEL_VER_FULL=$(uname -r)
+KERNEL_PACKAGE=linux-image-$_KERNEL_VER_FULL
+KERNEL_PKG_VERSION=`dpkg-query --showformat='${Version}' --show $KERNEL_PACKAGE`
+KERNEL_VERSION_PKG_SMALL=$(echo $KERNEL_PKG_VERSION | sed -r "s/[0-9]:([0-9][.][0-9]{1,2}[.][0-9]{1,3})[-].*/\1/")
+echo "Kernel $KERNEL_VERSION_PKG_SMALL detected, try to install drivers"
+apt-get install -y "aloop-$KERNEL_VERSION_PKG_SMALL" "pcm1794a-$KERNEL_VERSION_PKG_SMALL" "rtl88xxau-$KERNEL_VERSION_PKG_SMALL"
