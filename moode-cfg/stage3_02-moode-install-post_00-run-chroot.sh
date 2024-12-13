330a3fef4e59bcc6e5b1f59e18c78e9571addade
Tim Curtis <tim@moodeaudio.org>
2024-12-13 16:17:38 -0500

    Update stage3_02-moode-install-post_00-run-chroot.sh
    Add section to install specific chromium-browser version and then apply holds on chromium packages to prevent downstream pi-gen processes from installing latest ver.

--- moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
+++ moode-cfg/stage3_02-moode-install-post_00-run-chroot.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 ################################################################################
 #
-# Install kernel drivers
+# Post moode install
 #
-# (C) bitkeeper 2022 http://moodeaudio.org
+# Install kernel drivers
+# - aloop     384 kHz patch Ok
+# - pcm1794a  384 kHz patch Ok
+# - ax88179   Removed due to breakage in 6.1.yy, unmaintained by vendor (Allo)
+# - rtl88xxau Removed due to breakage in 6.6.31, unmaintained by Git repo owner
+#
+# Downgrade chromium
+# - Install version 126.0.6478.164-rpt1
+# - Apply package holds so downstream processes don't install newer chromium
+#
+# (C) 2022 @bitkeeper http://moodeaudio.org
+# (C) 2024 The moOde audio player project / Tim Curtis
 # License: GPLv3
 #
 ################################################################################
 
-#
-# aloop     384 kHz patch Ok
-# pcm1794a  384 kHz patch Ok
-# ax88179   Removed due to breakage in 6.1.yy, unmaintained by vendor (Allo)
-# rtl88xxau Removed due to breakage in 6.6.31, unmaintained by Git repo owner
-#
+################################################################################
+# Install kernel drivers
+################################################################################
 
 # Use code from rbl_get_kernel_source() for getting kernel version
 _KERNEL_VER_FULL=$(uname -r)
@@ -24,3 +32,15 @@ KERNEL_VERSION_PKG_SMALL=$(echo $KERNEL_PKG_VERSION | sed -r "s/[0-9]:([0-9][.][
 # Driver install
 echo "Kernel $KERNEL_VERSION_PKG_SMALL detected, try to install drivers"
 apt-get install -y "aloop-$KERNEL_VERSION_PKG_SMALL" "pcm1794a-$KERNEL_VERSION_PKG_SMALL"
+
+################################################################################
+# Install chromium v126
+################################################################################
+
+echo "Chromium, install v126"
+apt-get -y install chromium-browser=126.0.6478.164-rpt1 chromium-browser-l10n=126.0.6478.164-rpt1 chromium-codecs-ffmpeg-extra=126.0.6478.164-rpt1 --allow-downgrades
+apt-get -y purge chromium chromium-common chromium-sandbox rpi-chromium-mods
+apt-get -y autoremove
+echo "Chromium, apply package holds"
+apt-mark hold chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra
+apt-mark hold chromium chromium-common chromium-sandbox rpi-chromium-mods
