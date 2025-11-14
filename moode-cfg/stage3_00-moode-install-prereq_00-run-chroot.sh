39c37709403e3772eae9640bf2587acf4865ebba
bitkeeper <bitkeeper@users.noreply.github.com>
2025-11-14 17:05:06 +0100

    Switch from unstable to the main repo

--- moode-cfg/stage3_00-moode-install-prereq_00-run-chroot.sh
+++ moode-cfg/stage3_00-moode-install-prereq_00-run-chroot.sh
@@ -12,7 +12,7 @@ echo "Installing cloudsmith repository"
 
 curl -1sLf \
 'https://dl.cloudsmith.io/public/moodeaudio/m8y/setup.deb.sh' \
-| sudo -E distro=raspbian codename=trixie component=unstable bash
+| sudo -E distro=raspbian codename=trixie component=main bash
 
 echo "Update package lists"
 apt-get update
